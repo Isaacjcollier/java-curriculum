324a1f442a41e5a783b41d5fdb8665f794822514
Isaacjcollier <isaacjcollier@gmail.com>
2016-11-01 11:29:59 -0600

    init

--- Unit-1-Syntax/Practice.java
+++ Unit-1-Syntax/Practice.java
@@ -1,11 +1,34 @@
+import java.util.*;
+
 public class Practice {
 
   public static void main (String [] args) {
-    String [] myArray = new String [10];
-    System.out.println(myArray.length);
-
-    myArray[0] = "Hello";
-    myArray[11] = "World";
-    System.out.println(myArray[0]);
+    // create a HashmMap
+    HashMap<Integer, String> classMates = new HashMap<Integer, String>();
+    // Add everyone at your table with the key as your rating for how awesome they are
+    classMates.put(12, "Kristjan");
+    classMates.put(11, "George");
+    classMates.put(9, "Isaac");
+    classMates.put(8, "Derek");
+    //get one value and print it out
+    System.out.println(classMates.get(2));
+    // remove yourself
+    classMates.remove(9);
+    // print
+    System.out.println(classMates);
   }
 }
+
+// // Create a new ArrayList
+// ArrayList<String> names = new ArrayList<String>();
+// // Add everyone at your tables name to it
+// names.add("George");
+// names.add("Derek");
+// names.add("Isaac");
+// names.add("Kristjan");
+// // Print it out
+// System.out.println("All the names: " + names);
+// // remove yourself
+// names.remove("Isaac");
+// // Print it out again
+// System.out.println("All the names(without Isaac): " + names);
